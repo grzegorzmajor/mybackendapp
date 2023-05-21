e67be43f503781e60188df2b6963db5063648d3d
grzegorzmajor <major.grzegorz@gmail.com>
2023-05-21 21:25:13 +0200

    changed addedDate type to Date.

--- src/main/java/ovh/major/mybackendapp/domain/blog/dto/BlogPostResponseDTO.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/dto/BlogPostResponseDTO.java
@@ -2,12 +2,13 @@ package ovh.major.mybackendapp.domain.blog.dto;
 
 import lombok.Builder;
 
+import java.util.Date;
 import java.util.List;
 
 @Builder
 public record BlogPostResponseDTO(
         Integer id,
-        String addedDate,
+        Date addedDate,
         List<BlogPostParagraphResponseDTO> paragraphs
 ) {
 }
