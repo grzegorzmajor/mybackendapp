36991dfb0c5c164686c1306090734ececd0e5204
grzegorzmajor <major.grzegorz@gmail.com>
2023-05-21 21:25:49 +0200

    deleted addedDate

--- src/main/java/ovh/major/mybackendapp/domain/blog/dto/BlogPostRequestDTO.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/dto/BlogPostRequestDTO.java
@@ -3,6 +3,6 @@ package ovh.major.mybackendapp.domain.blog.dto;
 import java.util.List;
 
 public record BlogPostRequestDTO (
-        String addedDate,
+
         List<BlogPostParagraphRequestDTO> paragraphs
 ){}
