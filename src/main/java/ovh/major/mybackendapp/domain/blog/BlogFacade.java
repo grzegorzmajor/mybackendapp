5f926d8dc7ea9b46b5dd34419ae9d3217a23b1b5
grzegorzmajor <major.grzegorz@gmail.com>
2023-05-21 17:38:26 +0200

    added repositories and implements BlogMarkupDictionary method - find, save, delete

--- src/main/java/ovh/major/mybackendapp/domain/blog/BlogFacade.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/BlogFacade.java
@@ -1,11 +1,39 @@
 package ovh.major.mybackendapp.domain.blog;
 
 import lombok.AllArgsConstructor;
+import ovh.major.mybackendapp.domain.blog.dto.BlogMarkupDictionaryRequestDTO;
+import ovh.major.mybackendapp.domain.blog.dto.BlogMarkupDictionaryResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @AllArgsConstructor
 public class BlogFacade {
 
-    private final BlogRepository blogRepository;
+    private final BlogPostRepository blogPostRepository;
+    private final BlogPostParagraphRepository blogPostParagraphRepository;
+    private final BlogMarkupDictionaryRepository blogMarkupDictionaryRepository;
 
 
+    public List<BlogMarkupDictionaryResponseDTO> findAllMarkups() {
+        List<BlogMarkupDictionaryEntity> markups = StreamSupport.stream( blogMarkupDictionaryRepository.findAll().spliterator(), false)
+                .toList();
+        return markups.stream()
+                .map(BlogMarkupDictionaryMapper::mapToResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    public BlogMarkupDictionaryResponseDTO saveMarkup(BlogMarkupDictionaryRequestDTO requestDTO) {
+        return BlogMarkupDictionaryMapper.mapToResponseDto(
+                blogMarkupDictionaryRepository.save(
+                        BlogMarkupDictionaryMapper.mapFromRequestDto(requestDTO)
+                )
+        );
+
+    }
+
+    public void deleteMarkup(String id) {
+        blogMarkupDictionaryRepository.deleteById(Integer.parseInt(id));
+    }
 }
