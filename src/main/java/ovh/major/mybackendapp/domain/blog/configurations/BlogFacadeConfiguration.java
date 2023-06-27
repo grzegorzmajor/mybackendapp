34c261e17c0f63757810c3ff7d7343ff1a4affb4
grzegorzmajor <major.grzegorz@gmail.com>
2023-06-27 22:22:45 +0200

    moved to configurations package

--- src/main/java/ovh/major/mybackendapp/domain/blog/configurations/BlogFacadeConfiguration.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/configurations/BlogFacadeConfiguration.java
@@ -1,7 +1,8 @@
-package ovh.major.mybackendapp.domain.blog;
+package ovh.major.mybackendapp.domain.blog.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ovh.major.mybackendapp.domain.blog.BlogFacade;
 import ovh.major.mybackendapp.domain.blog.logic.MarkupDictionaryService;
 import ovh.major.mybackendapp.domain.blog.logic.ParagraphService;
 import ovh.major.mybackendapp.domain.blog.logic.PostService;
