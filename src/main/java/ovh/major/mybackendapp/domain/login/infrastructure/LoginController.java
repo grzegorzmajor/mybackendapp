745eec22ed94b056f027aeaad4a861e1c83eaf15
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-09-23 17:20:29 +0200

    added validation for method parameter and BadCredentials exception

--- src/main/java/ovh/major/mybackendapp/domain/login/infrastructure/LoginController.java
+++ src/main/java/ovh/major/mybackendapp/domain/login/infrastructure/LoginController.java
@@ -1,17 +1,20 @@
-package ovh.major.mybackendapp.infrastructure.login;
+package ovh.major.mybackendapp.domain.login.infrastructure;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import ovh.major.mybackendapp.domain.login.dto.UserRequestDTO;
 import ovh.major.mybackendapp.domain.login.dto.UserResponseDTO;
 import ovh.major.mybackendapp.infrastructure.security.jwt.JwtAuthenticatorFacade;
 
+import javax.validation.Valid;
+
 @RestController
 @Log4j2
 @AllArgsConstructor
-public class LoginController {
+class LoginController {
 
     private final JwtAuthenticatorFacade jwtAuthenticatorFacade;
     @PostMapping("/login")
@@ -19,7 +22,7 @@ public class LoginController {
             origins = "*",
             methods = RequestMethod.POST
     )
-    public ResponseEntity<UserResponseDTO> authenticateUser(@RequestBody UserRequestDTO userRequestDTO) {
+    public ResponseEntity<UserResponseDTO> authenticateUser(@RequestBody @Valid UserRequestDTO userRequestDTO) throws BadCredentialsException {
         final UserResponseDTO userResponse = jwtAuthenticatorFacade.authenticateAndGenerateToken(userRequestDTO);
         return ResponseEntity.ok(userResponse);
     }
