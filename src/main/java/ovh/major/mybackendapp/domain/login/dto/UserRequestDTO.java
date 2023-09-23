501a13cae36b9d1124e8aa44033ccf931df27277
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-09-23 17:27:31 +0200

    added @NotBlank annotation

--- src/main/java/ovh/major/mybackendapp/domain/login/dto/UserRequestDTO.java
+++ src/main/java/ovh/major/mybackendapp/domain/login/dto/UserRequestDTO.java
@@ -2,9 +2,15 @@ package ovh.major.mybackendapp.domain.login.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder
 public record UserRequestDTO(
+
+        @NotBlank(message = "Username must be not blank!")
         String name,
+
+        @NotBlank(message = "password must be not blank!")
         String password
 ) {
 }
