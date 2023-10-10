c718e8f361fd19ef74d914a2838ebb89329e10f1
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-10 14:50:11 +0200

    added encoderConfig for rest assured

--- src/integration/java/ovh/major/mybackendapp/DBIntegrationTest.java
+++ src/integration/java/ovh/major/mybackendapp/DBIntegrationTest.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.restassured.config.EncoderConfig.encoderConfig;
+
 
 @SpringBootTest(classes = MyBackendApplication.class)
 public class DBIntegrationTest {
@@ -11,6 +13,7 @@ public class DBIntegrationTest {
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "http://localhost";
+        RestAssured.config().encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
     }
 
 }
