a1b198427016b2384919367452db669b35d2c6f5
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-10 16:05:26 +0200

    added handler for DuplicatedTagException

--- src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/error/ControllerErrorHandler.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/error/ControllerErrorHandler.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import ovh.major.mybackendapp.domain.blog.infrastructure.exception.DuplicatedTagInDictException;
 import ovh.major.mybackendapp.domain.blog.infrastructure.exception.NoUsedTagInDictDBException;
 
 @ControllerAdvice
 class ControllerErrorHandler {
 
     private static final String NO_TAG_IN_DICT = "Something goes wrong! Maybe there is no used tag in dictionary.";
+    private static final String DUPLICATED_TAG = "Something goes wrong! Tag already exist in dictionary.";
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoUsedTagInDictDBException.class)
@@ -18,4 +20,12 @@ class ControllerErrorHandler {
     public ErrorResponse handleUsedTagInDictDBException() {
         return new ErrorResponse(NO_TAG_IN_DICT, HttpStatus.BAD_REQUEST);
     }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DuplicatedTagInDictException.class)
+    @ResponseBody
+    public ErrorResponse handleDuplicatedTagInDictException() {
+        return new ErrorResponse(DUPLICATED_TAG, HttpStatus.CONFLICT);
+    }
+
 }
