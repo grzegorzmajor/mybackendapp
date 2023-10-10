468f9c1e506b42fb0cc57814a1036f88f6010dd1
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-10 17:22:16 +0200

    added method to get post with publication date in the future

--- src/integration/java/ovh/major/mybackendapp/Examples.java
+++ src/integration/java/ovh/major/mybackendapp/Examples.java
@@ -1,5 +1,7 @@
 package ovh.major.mybackendapp;
 
+import java.sql.Timestamp;
+
 public enum Examples {
     BAD_CREDENTIALS("{\"name\":\"badusername\",\"password\":\"badpass\"}"),
     CORRECT_CREDENTIALS("{\n" +
@@ -8,6 +10,28 @@ public enum Examples {
             "}"),
     POST("{\n" +
             "    \"publicationDate\":\"2023-10-08T00:00:00.000+00:00\",\n" +
+            "    \"paragraphs\": [        \n" +
+            "        {            \n" +
+            "            \"tag\": {\n" +
+            "                    \"id\":\"\",\n" +
+            "                    \"opening\": \"<h3 class=\\\"blog__h3\\\">\",\n" +
+            "                    \"closing\": \"</h3>\"\n" +
+            "                },\n" +
+            "            \"paragraphContent\": \"Pierwsze testy integracyjne\"\n" +
+            "        },        \n" +
+            "        {            \n" +
+            "            \"tag\": {\n" +
+            "                    \"id\":\"\",\n" +
+            "                    \"opening\": \"<h3 class=\\\"blog__h3\\\">\",\n" +
+            "                    \"closing\": \"</h3>\"\n" +
+            "                },\n" +
+            "            \"paragraphContent\": \"Czas już było wrócić do mojego backendu do bloga.\" \n" +
+            "        }\n" +
+            "    ]\n" +
+            "}"),
+    POST_BEFORE_PUBLICATION_DATE("{\n" +
+            "    \"publicationDate\":\""),
+    POST_AFTER_PUBLICATION_DATE("\",\n" +
             "    \"paragraphs\": [        \n" +
             "        {            \n" +
             "            \"tag\": {\n" +
@@ -50,5 +74,10 @@ public enum Examples {
 
     public String toString() {
         return value;
+    }
+
+    static String getInTheFuturePost() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis() + 3600000);
+        return POST_BEFORE_PUBLICATION_DATE + timestamp.toString().replace(" ", "T") + "+00:00" + POST_AFTER_PUBLICATION_DATE;
     }
 }
