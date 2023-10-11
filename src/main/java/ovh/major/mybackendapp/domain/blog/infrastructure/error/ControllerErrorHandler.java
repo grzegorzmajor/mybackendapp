60993f45202df2aff2921286dc9f163c90587e96
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-11 14:16:26 +0200

    added new handler for DependencyExistException

--- src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/error/ControllerErrorHandler.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/error/ControllerErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import ovh.major.mybackendapp.domain.blog.infrastructure.exception.DependencyExistsException;
 import ovh.major.mybackendapp.domain.blog.infrastructure.exception.DuplicatedTagInDictException;
 import ovh.major.mybackendapp.domain.blog.infrastructure.exception.NoUsedTagInDictDBException;
 
@@ -13,6 +14,7 @@ class ControllerErrorHandler {
 
     private static final String NO_TAG_IN_DICT = "Something goes wrong! Maybe there is no used tag in dictionary.";
     private static final String DUPLICATED_TAG = "Something goes wrong! Tag already exist in dictionary.";
+    private static final String DELETE_NOT_POSSIBLE_BECAUSE_DEPENDENCY_IS_EXIST = "Something goes wrong! Deleting not possible because dependency is exist.";
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoUsedTagInDictDBException.class)
@@ -26,6 +28,13 @@ class ControllerErrorHandler {
     @ResponseBody
     public ErrorResponse handleDuplicatedTagInDictException() {
         return new ErrorResponse(DUPLICATED_TAG, HttpStatus.CONFLICT);
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DependencyExistsException.class)
+    @ResponseBody
+    public ErrorResponse handleDependencyExistException() {
+        return new ErrorResponse(DELETE_NOT_POSSIBLE_BECAUSE_DEPENDENCY_IS_EXIST,HttpStatus.CONFLICT);
     }
 
 }
