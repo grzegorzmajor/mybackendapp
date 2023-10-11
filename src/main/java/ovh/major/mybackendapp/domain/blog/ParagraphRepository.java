a295219146fc04128bc6af83a37e9764bcfff8b4
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-11 14:14:59 +0200

    added method to check dependencies for specified tag from dictionary

--- src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphRepository.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphRepository.java
@@ -1,8 +1,14 @@
 package ovh.major.mybackendapp.domain.blog;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 interface ParagraphRepository extends CrudRepository<ParagraphEntity, Integer> {
     ParagraphEntity findFirstById(Integer id);
+
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM ParagraphEntity p WHERE p.tag.id = :dictionaryId")
+    boolean checkDependencies(@Param("dictionaryId") Integer dictionaryId);
+
 }
