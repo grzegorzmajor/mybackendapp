574a182bd4b69ef10d8d703fdfcee855c67e2bea
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 17:44:48 +0200

    added deleting markup when there is no dependency to it after patch and delete paragraph

--- src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphService.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphService.java
@@ -3,6 +3,7 @@ package ovh.major.mybackendapp.domain.blog;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ovh.major.mybackendapp.domain.blog.dto.ParagraphRequestDTO;
 import ovh.major.mybackendapp.domain.blog.dto.ParagraphResponseDTO;
@@ -29,9 +30,14 @@ class ParagraphService {
             throw new EntityWithIdNotExistException(requestDTO.id());
         }
 
+
+
         ParagraphEntity fromDatabaseParagraphEntity = paragraphRepository.findFirstById(requestDTO.id());
 
         ParagraphEntity newParagraphEntity = ParagraphMapper.mapFromRequestDto(requestDTO);
+
+        MarkupDictionaryEntity oldMarkupDictionaryEntity = fromDatabaseParagraphEntity.getTag();
+
 
         if (!fromDatabaseParagraphEntity
                 .getTag()
@@ -61,7 +67,11 @@ class ParagraphService {
 
         if (!isChanged) throw new NothingToChangeException();
 
-        return ParagraphMapper.mapToResponseDto(paragraphRepository.save(fromDatabaseParagraphEntity));
+        ParagraphEntity savedParagraphEntity = paragraphRepository.save(fromDatabaseParagraphEntity);
+
+        deleteMarkupIfDependenciesNotExist(oldMarkupDictionaryEntity.getId());
+
+        return ParagraphMapper.mapToResponseDto(savedParagraphEntity);
     }
 
     @Transactional
@@ -81,6 +91,17 @@ class ParagraphService {
             throw new EntityWithIdNotExistException(id);
         }
 
+        ParagraphEntity deletingParagraph = paragraphRepository.findFirstById(id);
+
         paragraphRepository.deleteById(id);
+
+        deleteMarkupIfDependenciesNotExist(deletingParagraph.getTag().getId());
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void deleteMarkupIfDependenciesNotExist(Integer deletingParagraph) {
+        if (!paragraphRepository.checkDependencies(deletingParagraph)) {
+            markupDictionaryRepository.deleteById(deletingParagraph);
+        }
     }
 }
