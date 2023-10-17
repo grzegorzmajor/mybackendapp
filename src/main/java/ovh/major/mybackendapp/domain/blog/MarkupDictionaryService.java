de418d480cd11ce7a68f1da4f9d98bcf49049505
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 17:37:10 +0200

    deleted unnecessary method, refactored

--- src/main/java/ovh/major/mybackendapp/domain/blog/MarkupDictionaryService.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/MarkupDictionaryService.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import ovh.major.mybackendapp.domain.blog.dto.MarkupDictionaryRequestDTO;
 import ovh.major.mybackendapp.domain.blog.dto.MarkupDictionaryResponseDTO;
-import ovh.major.mybackendapp.domain.blog.infrastructure.exception.DependencyExistsException;
 import ovh.major.mybackendapp.domain.blog.infrastructure.exception.DuplicatedTagInDictException;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Log4j2
@@ -16,18 +14,22 @@ import java.util.stream.StreamSupport;
 class MarkupDictionaryService {
 
     private final MarkupDictionaryRepository markupDictionaryRepository;
-    private final ParagraphRepository paragraphRepository;
 
     public List<MarkupDictionaryResponseDTO> findAllMarkups() {
-        List<MarkupDictionaryEntity> markups = StreamSupport.stream(markupDictionaryRepository.findAll().spliterator(), false)
+        List<MarkupDictionaryEntity> markups = StreamSupport.stream(
+                markupDictionaryRepository.findAll()
+                        .spliterator(), false)
                 .toList();
+
         return markups.stream()
                 .map(MarkupDictionaryMapper::mapToResponseDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public MarkupDictionaryResponseDTO saveMarkup(MarkupDictionaryRequestDTO requestDTO) {
+
         MarkupDictionaryEntity markupDictionaryEntity = markupDictionaryRepository.findFirstByOpening(requestDTO.opening());
+
         if (markupDictionaryEntity != null) {
             throw new DuplicatedTagInDictException();
         }
@@ -36,14 +38,5 @@ class MarkupDictionaryService {
                         MarkupDictionaryMapper.mapFromRequestDto(requestDTO)
                 )
         );
-
     }
-
-    public void deleteMarkup(Integer id) {
-        if (paragraphRepository.checkDependencies(id)) {
-            throw new DependencyExistsException();
-        }
-        markupDictionaryRepository.deleteById(id);
-    }
-
 }
