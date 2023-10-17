99e72ed1a2a22db1bfe33fef0804c067f3c91048
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 17:42:12 +0200

    added new method for delete paragraph

--- src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphRepository.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphRepository.java
@@ -1,14 +1,20 @@
 package ovh.major.mybackendapp.domain.blog;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 
 interface ParagraphRepository extends CrudRepository<ParagraphEntity, Integer> {
+
     ParagraphEntity findFirstById(Integer id);
 
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM ParagraphEntity p WHERE p.tag.id = :dictionaryId")
     boolean checkDependencies(@Param("dictionaryId") Integer dictionaryId);
+
+    @Modifying
+    @Query("DELETE FROM ParagraphEntity p WHERE p.id = :id")
+    void deleteById(@Param("id") Integer id);
 
 }
