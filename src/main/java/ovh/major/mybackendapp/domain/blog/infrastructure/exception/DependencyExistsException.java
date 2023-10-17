280e0b2c0bf9d52b176a7a6378bece9be260cde8
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 15:11:37 +0200

    deleted unnecessary exception class

--- src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/exception/DependencyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ovh.major.mybackendapp.domain.blog.infrastructure.exception;
-
-public class DependencyExistsException extends RuntimeException {
-    public DependencyExistsException() {
-        super("ERROR: Deleting not possible because dependency is exist.");
-    }
-}
