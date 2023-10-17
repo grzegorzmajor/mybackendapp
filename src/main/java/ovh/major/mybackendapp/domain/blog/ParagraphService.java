51fb91ddc64cf628bbcaea4e5235a86e4ab6fa27
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 15:22:37 +0200

    refactored, changed implementation of patchParagraph method, added exceptions for better error feedback

--- src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphService.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/ParagraphService.java
@@ -2,35 +2,85 @@ package ovh.major.mybackendapp.domain.blog;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import ovh.major.mybackendapp.domain.blog.dto.ParagraphRequestDTO;
 import ovh.major.mybackendapp.domain.blog.dto.ParagraphResponseDTO;
+import ovh.major.mybackendapp.domain.blog.infrastructure.exception.EntityWithIdNotExistException;
+import ovh.major.mybackendapp.domain.blog.infrastructure.exception.NoUsedTagInDictDBException;
+import ovh.major.mybackendapp.domain.blog.infrastructure.exception.NothingToChangeException;
 
 @Log4j2
 @AllArgsConstructor
 class ParagraphService {
 
+    @Autowired
     private final MarkupDictionaryRepository markupDictionaryRepository;
+
+    @Autowired
     private final ParagraphRepository paragraphRepository;
 
+    @Transactional
     public ParagraphResponseDTO patchParagraph(ParagraphRequestDTO requestDTO) {
 
-        ParagraphEntity responseFromDatabaseParagraphEntity = paragraphRepository.findFirstById(requestDTO.id());
+        boolean isChanged = false;
 
-        ParagraphEntity postParagraphEntity = ParagraphMapper.mapFromRequestDto(requestDTO);
+        if (!paragraphRepository.existsById(requestDTO.id())) {
+            throw new EntityWithIdNotExistException(requestDTO.id());
+        }
 
-        responseFromDatabaseParagraphEntity.setTag(postParagraphEntity.getTag());
-        responseFromDatabaseParagraphEntity.setParagraphContent(postParagraphEntity.getParagraphContent());
+        ParagraphEntity fromDatabaseParagraphEntity = paragraphRepository.findFirstById(requestDTO.id());
 
-        MarkupDictionaryEntity responseFromDatabaseMarkupDictionaryEntity =
-                markupDictionaryRepository.findFirstByOpening(responseFromDatabaseParagraphEntity.getTag().getOpening());
+        ParagraphEntity newParagraphEntity = ParagraphMapper.mapFromRequestDto(requestDTO);
 
-        responseFromDatabaseParagraphEntity.setTag(responseFromDatabaseMarkupDictionaryEntity);
+        if (!fromDatabaseParagraphEntity
+                .getTag()
+                .getOpening()
+                .equals(newParagraphEntity
+                        .getTag()
+                        .getOpening())) {
 
-        return ParagraphMapper.mapToResponseDto(paragraphRepository.save(responseFromDatabaseParagraphEntity));
+            MarkupDictionaryEntity markupDictionaryEntity =
+                    markupDictionaryRepository.findFirstByOpening(newParagraphEntity
+                            .getTag()
+                            .getOpening());
+
+            if (markupDictionaryEntity == null) throw new NoUsedTagInDictDBException();
+
+            fromDatabaseParagraphEntity.setTag(markupDictionaryEntity);
+            isChanged = true;
+        }
+
+        if (!fromDatabaseParagraphEntity.getParagraphContent()
+                .equals(newParagraphEntity.getParagraphContent())) {
+            fromDatabaseParagraphEntity.setParagraphContent(
+                    newParagraphEntity.getParagraphContent());
+
+            isChanged = true;
+        }
+
+        if (!isChanged) throw new NothingToChangeException();
+
+        return ParagraphMapper.mapToResponseDto(paragraphRepository.save(fromDatabaseParagraphEntity));
     }
 
-    public ParagraphResponseDTO getParagraph(String id) {
-        return ParagraphMapper.mapToResponseDto(paragraphRepository.findFirstById(Integer.parseInt(id)));
+    @Transactional
+    public ParagraphResponseDTO getParagraph(Integer id) {
+
+        if (!paragraphRepository.existsById(id)) {
+            throw new EntityWithIdNotExistException(id);
+        }
+
+        return ParagraphMapper.mapToResponseDto(paragraphRepository.findFirstById(id));
     }
 
+    @Transactional
+    public void deleteParagraphById(Integer id) {
+
+        if (!paragraphRepository.existsById(id)) {
+            throw new EntityWithIdNotExistException(id);
+        }
+
+        paragraphRepository.deleteById(id);
+    }
 }
