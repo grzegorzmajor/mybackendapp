e591465f7b8d63c383e79a2bffad006f12566b4a
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 15:19:14 +0200

    added PATCH, DELETE and GET endpoints (GET for single post with specified id)

--- src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/PostController.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/infrastructure/PostController.java
@@ -5,11 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ovh.major.mybackendapp.domain.blog.BlogFacade;
 import ovh.major.mybackendapp.domain.blog.dto.PostRequestDTO;
 import ovh.major.mybackendapp.domain.blog.dto.PostResponseDTO;
+
+import java.sql.Timestamp;
+
 
 @RestController
 @Log4j2
@@ -28,17 +32,35 @@ class PostController {
         Page<PostResponseDTO> postsPage = blogFacade.findAllForPublicationPostsPageable(pageable);
         return ResponseEntity.ok(postsPage);
     }
-    @GetMapping(value="/with-unpublished", params = {"page", "size"})
+
+    @GetMapping(value = "/with-unpublished", params = {"page", "size"})
     public ResponseEntity<Page<PostResponseDTO>> findAllFPostsPageable(Pageable pageable) {
         Page<PostResponseDTO> postsPage = blogFacade.findAllPostsPageable(pageable);
         return ResponseEntity.ok(postsPage);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PostResponseDTO> findPostById(@PathVariable Integer id) {
+        PostResponseDTO post = blogFacade.findPostById(id);
+        return ResponseEntity.ok(post);
+    }
+
     @PostMapping
     public ResponseEntity<PostResponseDTO> addPost(@RequestBody PostRequestDTO requestDTO) {
-        PostResponseDTO responseDTO;
-        responseDTO = blogFacade.savePost(requestDTO);
+        PostResponseDTO responseDTO = blogFacade.savePost(requestDTO);
         return ResponseEntity.ok(responseDTO);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> patchPost(@PathVariable Integer id, @RequestParam String timestamp) {
+        PostResponseDTO responseDTO = blogFacade.patchPost(id, Timestamp.valueOf(timestamp.replace("T"," ")));
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable Integer id) {
+        blogFacade.deletePostById(id);
     }
 
 }
