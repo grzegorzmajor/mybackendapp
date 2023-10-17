55dd183d8f59e116cf140b0cd36886a7e8236083
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 15:16:41 +0200

    added new method updatePublicationDate

--- src/main/java/ovh/major/mybackendapp/domain/blog/PostRepository.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/PostRepository.java
@@ -3,6 +3,7 @@ package ovh.major.mybackendapp.domain.blog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,4 +14,7 @@ interface PostRepository extends JpaRepository<PostEntity, Integer> {
     @Query("SELECT e FROM PostEntity e WHERE e.publicationDate <= :date")
     Page<PostEntity> findAllWithPublicationDateTest(@Param("date") Timestamp date, Pageable pageable);
 
+    @Modifying
+    @Query("UPDATE PostEntity p SET p.publicationDate = :timestamp WHERE p.id = :id")
+    Integer updatePublicationDate(@Param("id") Integer id, @Param("timestamp") Timestamp timestamp);
 }
