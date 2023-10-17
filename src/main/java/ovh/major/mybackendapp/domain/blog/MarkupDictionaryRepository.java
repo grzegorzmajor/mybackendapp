bcad7e2cd2971d28b2165548620c8c112c21b3e8
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-17 17:36:08 +0200

    added delete method

--- src/main/java/ovh/major/mybackendapp/domain/blog/MarkupDictionaryRepository.java
+++ src/main/java/ovh/major/mybackendapp/domain/blog/MarkupDictionaryRepository.java
@@ -1,9 +1,17 @@
 package ovh.major.mybackendapp.domain.blog;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 interface MarkupDictionaryRepository extends CrudRepository<MarkupDictionaryEntity, Integer> {
 
     MarkupDictionaryEntity findFirstByOpening(String opening);
+
+    @Modifying
+    @Query("DELETE MarkupDictionaryEntity m WHERE m.id = :id")
+    void deleteById(@Param("id") Integer id);
+
 }
